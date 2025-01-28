6245ad13359cea7e591c8328bc0bc9bd99814f43
AeronautYuFan <AeronautYuFan@gmail.com>
2025-01-28 11:16:34 -0500

    case 1

--- case study 1 code.R
+++ case study 1 code.R
@@ -4,15 +4,15 @@ library(Rfit)
 font_add(family = "ComputerModern", regular = "cmunrm.ttf")
 showtext_auto()
 #some of the code is for changing fonts. not necessary,
-#but it looks gourgeous in latex so why not
+#but it looks gorgeous in latex so why not
 
 hr <- read.table('hrPaired.txt', header = TRUE)
 hrDiffs <- hr$flightHR - hr$controlHR
 
-summary(hr)
+summary(hr) # summary stats
 sd(hr$controlHR)
 sd(hr$flightHR)
-summary(hrDiffs)
+summary(hrDiffs) # summary of differences
 sd(hrDiffs)
 
 hist(hrDiffs, main = "", col = "lightgreen",
@@ -27,5 +27,3 @@ t.test(hr$flightHR, hr$controlHR, paired = TRUE) #paired t test
 
 #wilcoxon sign test
 with(hr, wilcox.test(flightHR, controlHR, paired = TRUE, conf.int = TRUE)) #wilcoxon sign test
-
-HodgesLehmann(hrDiffs)
