15db55824cf857f6b22b0c7b9552503793d81f3e
AeronautYuFan <AeronautYuFan@gmail.com>
2025-02-12 10:48:04 -0500

    latex crash uh oh

--- homework2/case_study1.R
+++ homework2/case_study1.R
@@ -1,13 +1,25 @@
+#install.packages('DescTools')
+#install.packages('Rfit')
+library(ISwR) # data source
+
+# formatting stuff (add latex font)
+library(showtext)
+library(DescTools)
+library(Rfit)
+font_add(family = 'ComputerModern', regular = 'cmunrm.ttf') #for consistent formatting in LaTex
+showtext_auto()
+par(family = 'ComputerModern')
+# end formatting stuff
+
 # case study 1
-library(ISwR)
 
 data("vitcap2")
 
 # subsets of the different groups for histogram construction
+# this block creates dataframes to store the different treatment arms and also puts them in a list
 noExposure = subset(vitcap2, group == '3')
 midExposure = subset(vitcap2, group == '2')
 longExposure = subset(vitcap2, group == '1')
-
 exposures = list(vitcap2 = vitcap2, 
                  longExposure = longExposure,
                  midExposure = midExposure, 
@@ -33,10 +45,14 @@ for (group_name in names(exposures)) {
   print('')
 }
 
+# histogram of data
+hist(vitcap2$vital.capacity, col = "lightblue", 
+     breaks = 20, ylab = 'Number of Workers',
+     xlab = 'Observed Vital Capacity (Liters)',
+     main = '')
+# create a histogram from the total dataset
 
 
-
-hist(vitcap2$vital.capacity, col = "lightblue", breaks = 20)
 # 3 individuals
 hist(noExposure$vital.capacity, breaks = 20, col = rgb(0.68, 0.85, 0.90, alpha = 0.5)) #lightblue
 hist(midExposure$vital.capacity, breaks = 20, col = rgb(0.56, 0.93, 0.56, alpha = 0.5) , add = T)
@@ -47,10 +63,12 @@ summary(noExposure$vital.capacity)
 summary(midExposure$vital.capacity)
 summary(longExposure$vital.capacity)
 
-sd(vitcap2$vital.capacity)
-sd(longExposure$vital.capacity)
-sd(midExposure$vital.capacity)
-sd(noExposure$vital.capacity) 
+boxplot(vital.capacity ~ group, data = vitcap2, 
+        main = '',
+        xlab = 'Vital Capacity (in Liters)', 
+        ylab = 'Exposure to Cadmium', 
+        col = c('lightblue', 'lightgreen', 'beige'), horizontal = T,
+        names = c('Long Term', 'Short Term', 'Control'))
 
 kruskal.test(vital.capacity ~ group, data = vitcap2)
 
